e413b147f7eba135c99efc1e034edadcd08ea48d
Asif <asifbhat184@gmail.com>
2020-03-13 18:17:16 +0530

    [kotlin-2] : Example of while defining variable & while loop

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,10 @@
 fun main(){
     println("hello!This is my first program in Kotlin.")
+    var x =1
+    println("value of x = $x before loop")
+    while(x < 4) {
+        println("in loop. x= $x")
+        x =x+1
+    }
+    println("Outside loop.X = $x")
 }
